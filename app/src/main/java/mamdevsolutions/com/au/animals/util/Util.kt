760c328ee45dfa0fd39b1a0c10934a1d0a2e59cf
Manjesh Govindappa <manjesh.govindappa@ellume.com.au>
2020-10-31 17:06:06 +1000

    Add databinding for detail fragment

--- app/src/main/java/mamdevsolutions/com/au/animals/util/Util.kt
+++ app/src/main/java/mamdevsolutions/com/au/animals/util/Util.kt
@@ -1,10 +1,16 @@
 package mamdevsolutions.com.au.animals.util
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.palette.graphics.Palette
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import mamdevsolutions.com.au.animals.R
 
 fun getProgressDrawable(context: Context) :CircularProgressDrawable {
@@ -25,4 +31,9 @@ fun ImageView.loadImage(uri :String?, progressDrawable: CircularProgressDrawable
             .load(uri)
             .into(this)
 
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
 }
--- app/src/main/java/mamdevsolutions/com/au/animals/view/DetailFragment.kt
+++ app/src/main/java/mamdevsolutions/com/au/animals/view/DetailFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import androidx.palette.graphics.Palette
@@ -14,8 +15,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.item_animal.view.*
 import mamdevsolutions.com.au.animals.R
+import mamdevsolutions.com.au.animals.databinding.FragmentDetailBinding
 import mamdevsolutions.com.au.animals.model.Animal
+import mamdevsolutions.com.au.animals.model.AnimalPalette
 import mamdevsolutions.com.au.animals.util.getProgressDrawable
 import mamdevsolutions.com.au.animals.util.loadImage
 
@@ -28,13 +32,15 @@ import mamdevsolutions.com.au.animals.util.loadImage
 class DetailFragment : Fragment() {
 
     var animal: Animal? = null
+    private lateinit var dataBinding :FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,22 +49,14 @@ class DetailFragment : Fragment() {
         arguments?.let {
             animal = DetailFragmentArgs.fromBundle(it).animal
         }
-
-        context?.let {
-            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
-        }
-
-        animalName.text = animal?.name
-        animalLocation.text = animal?.location
-        animalLifespan.text = animal?.lifeSpan
-        animalDiet.text = animal?.diet
-
         animal?.imageUrl?.let {
             setupBackgroundColor(animal?.imageUrl)
         }
+        dataBinding.animal = animal
     }
 
-    private fun setupBackgroundColor(imageUrl: String?) {
+    fun setupBackgroundColor(imageUrl: String?) {
+
         Glide.with(this)
                 .asBitmap()
                 .load(imageUrl)
@@ -66,15 +64,13 @@ class DetailFragment : Fragment() {
                     override fun onLoadCleared(placeholder: Drawable?) {
 
                     }
-
                     override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                         Palette.from(resource)
                                 .generate() {palette ->
-                                    val intColor :Int = palette?.lightMutedSwatch?.rgb?:0
-                                    animalDetailLayout.setBackgroundColor(intColor)
+                                    var intColor = palette?.lightMutedSwatch?.rgb?:0
+                                    dataBinding.palette = AnimalPalette(intColor)
                                 }
                     }
                 })
     }
-
 }
